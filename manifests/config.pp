dee585b01e63ae76896d0707c81c5ab792943485
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-02-06 18:31:37 +0100

    clean up lint warnings

--- manifests/config.pp
+++ manifests/config.pp
@@ -37,37 +37,37 @@ define couchdb::config ($port=5984, $query_servers=[]) {
 
     include couchdb::install
 
-    file { "/etc/$name":
+    file { "/etc/${name}":
         ensure  => directory,
         owner   => $dirowner,
         group   => $group,
         mode    => $dirmode,
     }
 
-    file { "/etc/$name/local.ini":
+    file { "/etc/${name}/local.ini":
         ensure  => file,
         owner   => $owner,
         group   => $group,
         mode    => $mode,
         content => template('couchdb/local.ini.erb'),
         require => [
-            File["/etc/$name"],
-            File["/var/lib/$name"],
-            File["/var/log/$name"],
-            File["/etc/rc.d/init.d/$name"],
+            File["/etc/${name}"],
+            File["/var/lib/${name}"],
+            File["/var/log/${name}"],
+            File["/etc/rc.d/init.d/${name}"],
             Class['couchdb::install'],
         ],
-        notify => Couchdb::Service[$name],
+        notify  => Couchdb::Service[$name],
     }
 
-    file { "/var/lib/$name":
+    file { "/var/lib/${name}":
         ensure  => directory,
         owner   => $dirowner,
         group   => $group,
         mode    => $dirmode,
     }
 
-    file { "/var/log/$name":
+    file { "/var/log/${name}":
         ensure  => directory,
         owner   => $dirowner,
         group   => $group,
@@ -76,12 +76,12 @@ define couchdb::config ($port=5984, $query_servers=[]) {
 
     # if it's not the standard service name, symlink the new service script
     if $name != 'couchdb' {
-        file { "/etc/rc.d/init.d/$name":
+        file { "/etc/rc.d/init.d/${name}":
             ensure => link,
             target => 'couchdb',
         }
     } else {
-        file { "/etc/rc.d/init.d/$name":
+        file { "/etc/rc.d/init.d/${name}":
             require  => Class['couchdb::install'],
         }
     }
