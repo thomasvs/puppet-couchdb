a7c4152d7ff0a7d9ed37361658cd09976eab0be9
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-08-08 18:37:26 +0200

    pass admins

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,12 @@
-define couchdb ($port=5984, $query_servers=[]) {
+define couchdb (
+  $port=5984,
+  $query_servers=[],
+  $admins={}
+) {
   couchdb::config {$name:
     port          => $port,
     query_servers => $query_servers,
+    admins        => $admins,
   }
   couchdb::service {$name: }
 }
