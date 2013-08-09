83c31b164f6030558d20e76329741850827ce209
Thomas Vander Stichele <thomas (at) apestaart (dot) org>
2013-08-09 12:56:07 +0200

    add require_valid_user to main couchdb define

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,14 @@
 define couchdb (
   $port=5984,
   $query_servers=[],
-  $admins={}
+  $admins={},
+  $require_valid_user=undef
 ) {
   couchdb::config {$name:
-    port          => $port,
-    query_servers => $query_servers,
-    admins        => $admins,
+    port               => $port,
+    query_servers      => $query_servers,
+    admins             => $admins,
+    require_valid_user => $require_valid_user
   }
   couchdb::service {$name: }
 }
